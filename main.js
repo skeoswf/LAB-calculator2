aedb66a6cb55119e9dc6919b039adf122d0d14ef
Skeo <skeo051600@gmail.com>
2024-04-19 21:26:09 -0500

    Numbers now append to display

--- main.js
+++ main.js
@@ -1,1 +1,48 @@
-console.log("and look at me. i turned out just fine.")
+class Calculator {
+  constructor(displayOutput) {
+    this.displayOutput = displayOutput;
+    this.clear();
+  }
+
+  clear() {
+    this.displayOutput.textContent = ''
+    this.operation = null;
+  }
+
+  delete() {
+
+  }
+
+  appendNumber(number) {
+    this.displayOutput.textContent += number;
+  }
+
+  chooseOperation(operation) {
+
+  }
+
+  compute() {
+
+  }
+
+  updateDisplay() {
+    this.displayOutput;
+  }
+}
+
+
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationsButton = document.querySelectorAll('[data-operation');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-all-clear]')
+const displayOutput = document.querySelector('[data-output-display]');
+
+const calculator = new Calculator(displayOutput)
+
+numberButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    calculator.appendNumber(button.innerText)
+    calculator.updateDisplay()
+  })
+})
